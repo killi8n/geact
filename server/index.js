77f667ffc6cef9a186f5781b8fdbcfc9cffa6973
Dongho Choi <evals4dead@gmail.com>
2019-02-25 17:17:49 +0900

    build rendering on server side

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 import Koa from 'koa'
 import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
+import fs from 'fs'
+import path from 'path'
+import serve from 'koa-static'
 import api from './routes'
+
+const buildLocation = path.join(__dirname, '../build')
+const buildIndex = path.join(__dirname, '../build/index.html')
+const indexHtml = fs.readFileSync(buildIndex, { encoding: 'utf8' })
 
 const app = new Koa()
 const router = new Router()
@@ -11,6 +18,10 @@ router.use('/api', api.routes())
 app.use(bodyParser())
 
 app.use(router.routes()).use(router.allowedMethods())
+app.use(serve(buildLocation))
+app.use(ctx => {
+    ctx.body = indexHtml
+})
 
 app.listen(4000, () => {
     console.log('App is running on port 4000')
