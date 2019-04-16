2e73015ed3d4d538c6c1b000aecc0542257a528e
killi8n <evals4dead@gmail.com>
2019-04-16 20:44:39 +0900

    removed dist

--- geact-serverless/dist/api/dev/dev.ctrl.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ping = (ctx) => {
-    ctx.body = process.env.NODE_ENV;
-    ctx.status = 200;
-};
--- geact-serverless/dist/lib/accessToken.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.accessTokenMiddleware = (ctx, next) => {
-    const accessToken = ctx.cookies.get('access_token');
-    if (!accessToken)
-        return next();
-    ctx.request.access_token = accessToken;
-    return next();
-};
