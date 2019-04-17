f2e4f3d59499c3adf2518c98a146b21608771fc9
DonghoChoi <evals4dead@gmail.com>
2019-04-17 12:45:47 +0900

    push

--- geact-serverless/src/api/auth/auth.ctrl.ts
+++ geact-serverless/src/api/auth/auth.ctrl.ts
@@ -3,7 +3,6 @@ import Joi from 'joi';
 import axios from 'axios';
 
 export const login = async (ctx: Context) => {
-    console.log('login');
     const schema = Joi.object().keys({
         code: Joi.string().required(),
         client_id: Joi.string().required(),
@@ -41,22 +40,21 @@ export const login = async (ctx: Context) => {
         );
 
         const { data: user } = checkUserResponse;
+        const domain =
+            process.env.NODE_ENV === 'development'
+                ? 'localhost'
+                : process.env.REACT_APP_CLOUD_FRONT_URL;
+
         ctx.cookies.set('user', JSON.stringify(user), {
             httpOnly: true,
             maxAge: 1000 * 60 * 60 * 24 * 7,
-            domain:
-                process.env.NODE_ENV === 'development'
-                    ? 'localhost'
-                    : process.env.REACT_APP_CLOUD_FRONT_URL,
+            domain,
         });
         const { access_token } = response.data;
         ctx.cookies.set('access_token', access_token, {
             httpOnly: true,
             maxAge: 1000 * 60 * 60 * 24 * 7,
-            domain:
-                process.env.NODE_ENV === 'development'
-                    ? 'localhost'
-                    : process.env.REACT_APP_CLOUD_FRONT_URL,
+            domain,
         });
         ctx.body = {
             accessToken: response.data.access_token,
@@ -69,19 +67,18 @@ export const login = async (ctx: Context) => {
 };
 
 export const logout = (ctx: Context) => {
+    const domain =
+        process.env.NODE_ENV === 'development'
+            ? 'localhost'
+            : process.env.REACT_APP_CLOUD_FRONT_URL;
+
     ctx.cookies.set('user', '', {
         maxAge: 0,
-        domain:
-            process.env.NODE_ENV === 'development'
-                ? 'localhost'
-                : process.env.REACT_APP_CLOUD_FRONT_URL,
+        domain,
     });
     ctx.cookies.set('access_token', '', {
         maxAge: 0,
-        domain:
-            process.env.NODE_ENV === 'development'
-                ? 'localhost'
-                : process.env.REACT_APP_CLOUD_FRONT_URL,
+        domain,
     });
     ctx.status = 204;
 };
